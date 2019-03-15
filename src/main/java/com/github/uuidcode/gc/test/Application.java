c7fc975cfe31faed7acef92dcab09f2d5e2de56f
ted.song <ted.song@daumkakao.com>
2019-03-15 21:53:27 +0900

    UseParallelOldGC

--- src/main/java/com/github/uuidcode/gc/test/Application.java
+++ src/main/java/com/github/uuidcode/gc/test/Application.java
@@ -10,29 +10,68 @@ import java.util.stream.IntStream;
 public class Application {
     private Map<Long, List<Integer>> map = new HashMap<>();
 
+    private boolean fullGC = false;
+    private boolean callSystemGC = false;
+
+    public Application setCallSystemGC(boolean callSystemGC) {
+        this.callSystemGC = callSystemGC;
+        return this;
+    }
+
+    public Application setFullGC(boolean fullGC) {
+        this.fullGC = fullGC;
+        return this;
+    }
+
     public void run() {
         for (long i = 0; i < Long.MAX_VALUE; i++) {
             List<Integer> list = IntStream.range(0, 100_000)
                 .boxed()
                 .collect(Collectors.toList());
 
-            if (map.size() < 100) {
-                map.put(i, list);
-            } else {
-                if (new Random().nextBoolean()) {
-                    map.keySet().stream()
-                        .findFirst()
-                        .ifPresent(map::remove);
+            this.addData(i, list);
 
-//                    System.gc();
-                }
-            }
-
-            try {
-                Thread.sleep(10);
-            } catch (Throwable t) {
-            }
+            this.sleep();
         }
+    }
+
+    private void addData(long i, List<Integer> list) {
+        int mapSize = this.getMapSize();
+
+        if (map.size() < mapSize) {
+            map.put(i, list);
+        } else {
+            this.removeDataAndCallSystemGC();
+        }
+    }
+
+    private void removeDataAndCallSystemGC() {
+        if (new Random().nextBoolean()) {
+            return;
+        }
+
+        map.keySet().stream()
+            .findFirst()
+            .ifPresent(map::remove);
+
+        if (callSystemGC) {
+            System.gc();
+        }
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(10);
+        } catch (Throwable t) {
+        }
+    }
+
+    private int getMapSize() {
+        if (this.fullGC) {
+            return 1000;
+        }
+
+        return 100;
     }
 
     public static void main(String[] args){
---
+++ src/main/java/com/github/uuidcode/gc/test/UseParallelOldGC.java
@@ -0,0 +1,11 @@
+package com.github.uuidcode.gc.test;
+
+public class UseParallelOldGC extends GC {
+    public UseParallelOldGC() {
+        this.addVmOption("-XX:+UseParallelOldGC");
+    }
+
+    public static void main(String[] args) {
+        new UseParallelOldGC().run();
+    }
+}
