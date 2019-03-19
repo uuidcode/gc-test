6f064cb6e9b3af7dad69db5c4b1a316457e65954
uuidcode <uuidcode@gmail.com>
2019-03-19 16:15:35 +0900

    javaagent

--- src/main/java/com/github/uuidcode/gc/test/ObjectRunner.java
+++ src/main/java/com/github/uuidcode/gc/test/ObjectRunner.java
@@ -1,21 +1,17 @@
 package com.github.uuidcode.gc.test;
 
-import java.io.StringWriter;
-import java.nio.charset.StandardCharsets;
-
-import org.apache.commons.io.IOUtils;
 import org.github.jamm.MemoryMeter;
 
 import com.github.uuidcode.builder.process.JavaProcessBuilder;
 import com.github.uuidcode.util.CoreUtil;
 
 public class ObjectRunner {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         String memoryMeterJarPath = CoreUtil.getJarPath(MemoryMeter.class);
 
         String result = JavaProcessBuilder.of()
-            .addOption("-javaagent:" + memoryMeterJarPath)
-            .addClasspath("target/classes")
+            .javaagent(memoryMeterJarPath)
+            .addDefaultClasspath()
             .setClassName(ObjectSize.class)
             .runAndGetResult();
 
